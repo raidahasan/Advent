549b33ca458582fdb3063d0a5aa5f1b93ec039da
raidahasan <145690594+raidahasan@users.noreply.github.com>
2024-12-12 14:04:15 -0500

    Add files via upload

---
+++ Day2.java
@@ -0,0 +1,52 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day2 {
+    public static void main(String[] args) {
+        ArrayList<String> fileData = getFileData("src/InputFile");
+        int rows = fileData.size();
+        int columns = fileData.get(0).length();
+        String[][] grid = new String[rows][columns];
+
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[0].length; c++) {
+                grid[r][c] = fileData.get(r).substring(c, c+1);
+            }
+        }
+
+        int total = 0;
+
+        for (int r = 0; r < grid.length; r++) {
+            boolean safe = true;
+            for (int c = 0; c < grid[0].length-1; c++) {
+                if((Math.abs(Integer.parseInt(grid[r][c])-Integer.parseInt(grid[r][c+1])))>=3){
+                    safe = false;
+                }
+            }
+            if(safe){
+                total++;
+            }
+        }
+        System.out.println(total);
+    }
+
+    public static ArrayList<String> getFileData(String fileName) {
+        ArrayList<String> fileData = new ArrayList<String>();
+        try {
+            File f = new File(fileName);
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                if (!line.equals(""))
+                    fileData.add(line);
+            }
+            return fileData;
+        }
+        catch (FileNotFoundException e) {
+            return fileData;
+        }
+    }
+}
+
