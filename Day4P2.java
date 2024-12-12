b519884826d75528dceb7296fa273e686b7db963
raidahasan <145690594+raidahasan@users.noreply.github.com>
2024-12-12 13:51:48 -0500

    Add files via upload

--- Day4P2.java
+++ Day4P2.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Day4P2 {
@@ -22,34 +23,42 @@ public class Day4P2 {
             for (int c = 0; c < grid[0].length; c++) {
                 String letter = grid[r][c];
                 if (letter.equals("A")) {
-                    if(r>0 && c>0){
-                        if(grid[r-1][c-1]=="M" || grid[r-1][c-1]=="S"){
-                            if(r< grid.length-1 && c<grid[0].length-1){
-                                if(grid[r-1][c-1]=="M"){
-                                    if(grid[r+1][c+1]=="S"){
-                                        if(grid[r+1][c-1]=="M"){
-                                            if(grid[r+1][c+1]=="S"){
-
-                                            }
-                                        }
-                                    }
-                                }
-                                if(grid[r-1][c-1]=="S"){
-                                    if(grid[r+1][c+1]=="M"){
-
-                                    }
-                                }
-                            }
-                        }
+                    if(rightLeft(grid, r, c) && rightRight(grid, r, c)){
+                        total++;
                     }
                 }
             }
         }
         System.out.println(total);
     }
-    public boolean rightLeft (String[][] list, int r, int c){
-        if(r<list.length-1 && r>0 && c>0 && c<list[0].length-1 ){
+    public static boolean rightLeft(String[][] list, int r, int c){
+        if(r<list.length-1 && r>0 && c>0 && c<list[0].length-1){
+            if(Objects.equals(list[r - 1][c - 1], "M")){
+                if(Objects.equals(list[r + 1][c + 1], "S")){
+                    return true;
+                }
+            }
+            if(Objects.equals(list[r - 1][c - 1], "S")){
+                if(Objects.equals(list[r + 1][c + 1], "M")){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
+    public static boolean rightRight (String[][] list, int r, int c){
+        if(r<list.length-1 && r>0 && c>0 && c<list[0].length-1){
+            if(Objects.equals(list[r - 1][c + 1], "M")){
+                if(Objects.equals(list[r + 1][c - 1], "S")){
+                    return true;
+                }
+            }
+            if(Objects.equals(list[r - 1][c + 1], "S")){
+                if(Objects.equals(list[r + 1][c - 1], "M")){
+                    return true;
+                }
+            }
         }
         return false;
     }
